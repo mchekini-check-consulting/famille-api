0f4541490b7f8ba310f11afdf96f5d586635144f
mahdi chekini <me.chekini@gmail.com>
2022-10-16 17:53:03 +0200

    init backend project

---
+++ src/main/java/fr/checkconsulting/gardeenfant/GardeEnfantApplication.java
@@ -0,0 +1,13 @@
+package fr.checkconsulting.gardeenfant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeEnfantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardeEnfantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/checkconsulting/gardeenfant/GardeEnfantApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.checkconsulting.gardeenfant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeEnfantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
