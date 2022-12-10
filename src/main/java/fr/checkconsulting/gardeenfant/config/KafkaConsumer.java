b5465d7dd69d8c4ec0a37834f7bb368ea0173be5
mahdcheck <me.chekini@gmail.com>
2022-12-11 00:49:28 +0100

    bugfix intervention consumer

--- src/main/java/fr/checkconsulting/gardeenfant/config/KafkaConsumer.java
+++ src/main/java/fr/checkconsulting/gardeenfant/config/KafkaConsumer.java
@@ -1,10 +1,13 @@
 package fr.checkconsulting.gardeenfant.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.checkconsulting.gardeenfant.entity.Intervention;
 import fr.checkconsulting.gardeenfant.entity.Message;
 import fr.checkconsulting.gardeenfant.repository.ChatRepository;
 import fr.checkconsulting.gardeenfant.repository.InterventionRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -24,15 +27,18 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "chat-famille", groupId = "famille-group-id")
     public void listenSenderMessage(Message data) {
-        System.out.println("Message received by consumer : " + data);
+        log.info("Message received by consumer {}", data);
         // Sauvegarder une copie sur la base de données famille
         chatRepository.save(data);
     }
 
     @KafkaListener(topics = "intervention-app", groupId = "intervention-group-id")
-    public void listenSenderIntervention(Intervention data) {
-        //System.out.println("Intervention received by consumer : " + data);
+    public void listenSenderIntervention(ConsumerRecord data) throws JsonProcessingException {
+
+        Intervention intervention = new ObjectMapper().readValue(data.value().toString(), Intervention.class);
+
+        log.info("Message received by consumer {}", data.value());
         // Sauvegarder une copie sur la base de données famille
-        interventionRepository.save(data);
+        interventionRepository.save(intervention);
     }
 }
--- src/main/java/fr/checkconsulting/gardeenfant/entity/Intervention.java
+++ src/main/java/fr/checkconsulting/gardeenfant/entity/Intervention.java
@@ -1,6 +1,8 @@
 package fr.checkconsulting.gardeenfant.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +19,7 @@ import java.time.LocalDateTime;
 public class Intervention {
     @Id
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime timeIntervention;
     private Integer jour;
     private Integer matin;
