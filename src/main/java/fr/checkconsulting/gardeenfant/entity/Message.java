5eeb328fc4f0cb83e76e818cbcd5b8e8345139f9
mahdcheck <me.chekini@gmail.com>
2022-12-12 05:13:34 +0100

    Add LocalDateTimeDeserializer

--- src/main/java/fr/checkconsulting/gardeenfant/entity/Message.java
+++ src/main/java/fr/checkconsulting/gardeenfant/entity/Message.java
@@ -1,6 +1,8 @@
 package fr.checkconsulting.gardeenfant.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +20,7 @@ import java.time.LocalDateTime;
 public class Message {
     @Id
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime timeMessage;
     private String content;
     private String emailSource;
